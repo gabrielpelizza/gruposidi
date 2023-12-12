04fcb4ae91ffc661d76d5c3beba458ebc69e5cd9
leonelbenjamin <leonelbenjaminmercau@gmail.com>
2023-12-11 23:03:50 -0300

    midificacion 11/12

--- index.js
+++ index.js
@@ -31,6 +31,28 @@ window.addEventListener("scroll", function() {
     navbar.style.visibility = "hidden";
   }
 });
+ 
 
 
+//Swiper JS
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
